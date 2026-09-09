84d10edc2ea61be4e8631c3d73aba9d4c09834a4
marciokoba <mak.koba@gmail.com>
2026-09-09 17:17:39 +0900

    fix: alterei a logica da conta do valor

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 function calcularTotal(itens) {
     let total = 0
     for (let i = 0; i < itens.length; i++){
-        total += itens[i].preco
+        total += itens[i].preco 1.1
     }
     
     // aplica desconto de fidelidade
